f8b064280959c5cc66d4d41fb3aeab5e49319d04
monteiro1714 <joaomonteiro.1998@hotmail.com>
2025-06-05 19:45:26 +0100

    navBar

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import weatherRoutes from './routes/weather';
+import cors from 'cors';
 
 dotenv.config()
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
+app.use(cors({ origin: '*' })); // Allow all origins for CORS
 app.use(express.json());
 app.use('/api/weather', weatherRoutes)
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
