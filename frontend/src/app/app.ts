7af943907213b816102c25f12c3a342e369ac9c0
monteiro1714 <joaomonteiro.1998@hotmail.com>
2025-06-09 19:36:19 +0100

    Working on design, search bar etc

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from "./components/nav-bar/nav-bar";
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'frontend';
+  protected title = 'MeteoGo';
 }
--- frontend/src/app/pages/home-component/home-component.ts
+++ frontend/src/app/pages/home-component/home-component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { WeatherDisplayComponent } from "../../components/weather-display-component/weather-display-component";
+import { NavBar } from "../../components/nav-bar/nav-bar";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home-component',
-  imports: [WeatherDisplayComponent],
+  imports: [WeatherDisplayComponent, NavBar, Footer],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css'
 })
